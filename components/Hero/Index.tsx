a7fea632f0ba23d918a982c2d5802cef1354d1a1
qalbeali <qalbeali8@gmail.com>
2022-05-19 19:55:48 -0700

    updating header

--- components/Hero/Index.tsx
+++ components/Hero/Index.tsx
@@ -1,11 +1,44 @@
+import Image from 'next/image'
+import Images from '../../public/images/Images'
+// import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 export default function Hero() {
   return (
     <>
-      <div className=" text-center pt-[4rem] bg-gradient-to-br from-[#FF3E3D] to-[#466DFD] bg-clip-text text-2xl font-extrabold text-transparent ">
+      <div className=" bg-gradient-to-br from-[#FF3E3D] to-[#466DFD] bg-clip-text pt-[4rem] text-center text-[3rem] font-extrabold text-transparent ">
         Launchpad & DeFi hub of the Avalanche <br /> Ecosystem.
       </div>
-      <div className=" text-center text-sm py-2.5  text-white  text-transparent ">
-      The DeFi hub on AVALANCHE ecosystem to bootstrap innovations, monitor and manage digital assets.
+      <div className=" py-2.5 text-center text-sm  text-white  text-transparent ">
+        The DeFi hub on AVALANCHE ecosystem to bootstrap innovations, monitor
+        and manage digital assets.
+      </div>
+
+      <div className='py-10' style={{ backgroundImage: `url(Images/hero)` }}>
+        <div className="flex items-center justify-center space-x-4">
+          <div className="flex rounded-2xl border-2 border-blue-500 bg-transparent">
+            <div className="p-2">
+              <Image src={Images.b1} />
+            </div>
+            <div className="px-3 pt-8">
+              <div className="font-bold text-white ">DEX</div>
+              <div className="text-blue-500">Go PAD.FI's DEX</div>
+            </div>
+            <div className="cursor-pointer rounded-full pl-8 pt-8">
+              <Image src={Images.arrow} />
+            </div>
+          </div>
+          <div className="flex rounded-2xl border-2 border-blue-500 bg-transparent">
+            <div className="p-2">
+              <Image src={Images.b1} />
+            </div>
+            <div className="px-3 pt-8">
+              <div className="font-bold text-white ">DEX</div>
+              <div className="text-blue-500">Go PAD.FI's DEX</div>
+            </div>
+            <div className="cursor-pointer rounded-full pl-8 pt-8">
+              <Image src={Images.arrow} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- public/images/Images.tsx
+++ public/images/Images.tsx
@@ -1,9 +1,13 @@
 import box from "./box.png";
 import avax from "./avax.png";
 import litecoin from "./litecoin.png";
+import b1 from "./b1.png";
+import arrow from "./arrow.png";
 
 export default {
     box,
     avax,
-    litecoin
+    litecoin,
+    b1,
+    arrow,
 }
